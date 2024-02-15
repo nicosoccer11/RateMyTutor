f2d1ac6d684f4792cc5ccf51fae3b9f7a0a96e85
Tosin <insanechills@gmail.com>
2024-02-15 16:46:34 -0600

    Initial Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './navbar';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>React App</h1>
     </div>
   );
