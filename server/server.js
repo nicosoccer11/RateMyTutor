c7e9621dd049fd0cbe8a473872b939371a52165c
Nico Garcia <nicogarcia@client-10-229-54-77.tamulink.tamu.edu>
2024-02-20 15:42:56 -0600

    Changed server directory structure

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const userRoutes = require('./routes/userRoutes');
+//Add other routes here
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Use the user routes
+app.use(userRoutes);
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
-})
+});
