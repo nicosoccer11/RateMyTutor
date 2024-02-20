ee5e667127647552f90166141914d2c10e19cbff
Tosin <insanechills@gmail.com>
2024-02-20 15:15:27 -0600

    navbar

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -3,6 +3,13 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 
 function NavbarComp() {
     return (  
@@ -27,6 +34,20 @@ function NavbarComp() {
                             </NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
+                    <Form inline>
+                        <Row>
+                        <Col xs="auto">
+                            <Form.Control
+                            type="text"
+                            placeholder="Search"
+                            className=" mr-sm-2"
+                            />
+                        </Col>
+                        <Col xs="auto">
+                            <Button type="submit">Submit</Button>
+                        </Col>
+                        </Row>
+                    </Form>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
