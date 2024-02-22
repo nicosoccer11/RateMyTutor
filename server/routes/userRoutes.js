bcd407e10f66e87538fb68f8ca09267c893a24f4
Nico Garcia <nicogarcia@client-10-228-250-155.tamulink.tamu.edu>
2024-02-22 16:10:31 -0600

    lots of new things

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,10 @@
 // routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
+
 //Update here when add new route
-const { createUser, getAllUsers, getUserByUsername } = require('../controllers/usersController');
+const { createUser, getAllUsers, getUserByUsername, loginUser } = require('../controllers/usersController');
+
 // Route to create a new user
 router.post('/users', createUser);
 
@@ -10,7 +12,10 @@ router.post('/users', createUser);
 router.get('/users', getAllUsers);
 
 // Route to get a user by username   
-router.get('/users/:username', getUserByUsername); // Corrected this line
+router.get('/users/:username', getUserByUsername); 
+
+// Route to login a user
+router.post('/users/login', loginUser);
 
 // Define other user routes here
 
