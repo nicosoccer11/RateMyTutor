75fa8282029806fa0f0c0d1173e5df2b0bae7f8c
Tosin <insanechills@gmail.com>
2024-02-27 12:24:11 -0600

    Very Basic Chat UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './navbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css'
+import { ChakraProvider, theme}  from "@chakra-ui/react"
 
 function App() {
   return (
-    <div>
-        <Navbar />
-      <h1>React App</h1>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Navbar />
+    </ChakraProvider>
   );
 }
 
