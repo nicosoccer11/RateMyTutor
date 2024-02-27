aaa1b4f80e1b405f101be0f65d1cd835cbe9d7ba
vwin121 <vincentwen2003@gmail.com>
2024-02-27 17:02:46 -0600

    friend_database

--- server/routes/friendRoutes.js
+++ server/routes/friendRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Route to add a new friend
 router.post('/friends/add', addFriend);
 
-// Route to get all friends
-router.get('/friends/get', getFriends);
+// Route to add a new friend
+router.post('/friends/get', getFriends);
 
 module.exports = router;
