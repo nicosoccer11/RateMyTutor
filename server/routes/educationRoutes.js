c1faab3db0f28cd355e4bbf328689639fefbdbe2
Nico Garcia <nicogarcia@trapbook-air.local>
2024-02-28 15:00:32 -0600

    added isFriend to user, delete for qualifcation and education

--- server/routes/educationRoutes.js
+++ server/routes/educationRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addEducation, editEducation } = require('../controllers/educationController');
+const { addEducation, editEducation, deleteEducation } = require('../controllers/educationController');
 const router = express.Router();
 
 // Route to add a new education record
@@ -7,5 +7,8 @@ router.post('/education/add', addEducation);
 
 // Route to edit an existing education record
 router.patch('/education/edit/:educationID', editEducation);
+
+// Route to delete an existing education record
+router.delete('/education/delete/:educationID', deleteEducation);
 
 module.exports = router;
