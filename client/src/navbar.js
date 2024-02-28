801efc64c85290cd7aa20896459b6651fe5dbf66
Tosin <insanechills@gmail.com>
2024-02-28 15:01:04 -0600

    removing message from navbar

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -42,9 +42,9 @@ function NavbarComp({ setIsLoggedIn }) {
                                 <Nav.Link as={Link} to="/profile">
                                     Profile
                                 </Nav.Link>
-                                <Nav.Link as={Link} to="/messages">
+                                {/* <Nav.Link as={Link} to="/messages">
                                     Messages
-                                </Nav.Link>
+                                </Nav.Link> */}
                                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                     <NavDropdown.Item href="#action/3.2">
