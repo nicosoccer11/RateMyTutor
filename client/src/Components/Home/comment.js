4602327b0f19a09567468389e2d8b827a01218e4
Tosin <insanechills@gmail.com>
2024-03-28 15:14:30 -0500

    comment images

--- client/src/Components/Home/comment.js
+++ client/src/Components/Home/comment.js
@@ -1,8 +1,23 @@
 import { BrowserRouter as Router, Routes, Route, Link, useLocation, useNavigate } from 'react-router-dom';
 import image from './user.jpeg';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Comment = ({ commentid, postid, content, time,user }) => {
+    const [image, setImage] = useState("");
+
+
+    useEffect(() => {
+        const fetchPicture = async (user) => {
+            await axios.get(`http://localhost:5000/image/get/${user}`).then((response) => {
+                setImage(response.data.imageUrl);
+            })
+            }
+        
+        fetchPicture(user)
+    
+    }, [])
+
     return (
         <div className="commentbox">
             <img src={image} alt="user" className="user-image" />
