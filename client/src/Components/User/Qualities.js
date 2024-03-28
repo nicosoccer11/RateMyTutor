179d4ba3448da6c39dc80424597ae2eb17426339
Justin <justintv56@tamu.edu>
2024-03-28 16:30:15 -0500

    Added matching qualities

--- client/src/Components/User/Qualities.js
+++ client/src/Components/User/Qualities.js
@@ -13,7 +13,7 @@ function Qualities({ qualities, setQualities, user }) {
         }
         try {
             const updatedQualities = [...qualities];
-            if(updatedQualities[qualityIndex - 1].hasquality === 0){
+            if (updatedQualities[qualityIndex - 1].hasquality === 0) {
                 const response = await axios.post('http://localhost:5000/user-qualities/add', {
                     username: user,
                     qualityId: qualityIndex
@@ -42,11 +42,26 @@ function Qualities({ qualities, setQualities, user }) {
                 {qualities && qualities.length > 0 && qualities.map((quality) => (
                     <button
                         key={quality.qualityid}
-                        className={`quality-item ${quality.hasquality ? 'active' : ''}`}
+                        className={`quality-item ${quality.hasquality ? (quality.hasmatch && !isCurrentUser ? 'match' : 'active ') : ''}`}
                         onClick={() => handleQualityToggle(quality.qualityid)}
                     >
-                        {quality.hasquality ? (<span className="checkmark">&#10003;{quality.qualityname}</span>) :
-                            (<span>{quality.qualityname}</span>)}
+                        {isCurrentUser ? (
+                            quality.hasquality ? (
+                                <span className="checkmark">{quality.qualityname}</span>
+                            ) : (
+                                <span>{quality.qualityname}</span>
+                            )
+                        ) : (
+                            quality.hasmatch ? (
+                                <span className="matched-quality">{quality.qualityname} &#10003;</span>
+                            ) : (
+                                quality.hasquality ? (
+                                    <span className="checkmark">{quality.qualityname}</span>
+                                ) : (
+                                    <span>{quality.qualityname}</span>
+                                )
+                            )
+                        )}
                     </button>
                 ))}
             </div>
