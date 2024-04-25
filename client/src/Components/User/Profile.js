031acfc43d24532a115f9538e300a5bbd1439667
Justin <justintv56@tamu.edu>
2024-04-25 14:45:19 -0500

    CSS changes

--- client/src/Components/User/Profile.js
+++ client/src/Components/User/Profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ProfileInfo from './ProfileInfo';
 import Reviews from './Reviews';
-import './Profile.css';
+import './Profile.css'
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import Posts from './Posts';
